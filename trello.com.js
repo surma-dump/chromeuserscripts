da02114aeb723c02c6d9d59e14c72cc7ab8572c6
Alexander Surma <alexander.surma@gmail.com>
2011-12-14 14:02:32 +0100

    Decoupling and "Load All" button

--- trello.com.js
+++ trello.com.js
@@ -1,12 +1,49 @@
-function highlight() {
-	$('#content .list-notifications > div > div').each(function() {
-		if($(this).hasClass('read')) {
-			$(this).css('background-color', 'transparent');
-		} else {
-			$(this).css('background-color', 'rgba(255,0,0,0.5)');
-		}
-	});
-	setTimeout(highlight, 4000);
-}
-highlight();
+(function () {
+	var getNotifications = function() {
+		return $('#content .list-notifications .phenom');
+	}
+	var highlight = function() {
+		getNotifications().each(function() {
+			if($(this).hasClass('read')) {
+				$(this).css('background-color', 'transparent');
+			} else {
+				$(this).css('background-color', 'rgba(255,0,0,0.5)');
+			}
+		});
+		setTimeout(highlight, 4000);
+	};
+
+	var createOpenAllButton = function() {
+		var $button = $('<div>')
+				.text('Load all')
+				.css({
+				'background-color': '#1D6087',
+				'position': 'fixed',
+				'bottom': '0px',
+				'right': '0px',
+				'padding': '10px'
+		});
+		$button.click(function() {
+			var list = new Array();
+			getNotifications().each(function() {
+				if(!$(this).hasClass('read')) {
+					var link = $(this).find('.phenom-desc > a.action-card').attr('href');
+					if($.inArray(link, list) == -1) {
+						list.push(link);
+					}
+				}
+			})
+			$.each(list, function(idx, item) {
+				window.open(item);
+			})
+			setTimeout(function() {location.reload();}, 1000);
+		})
+		$('body').append($button);
+	}
+
+	highlight();
+	createOpenAllButton();
+})();
+
+
 
